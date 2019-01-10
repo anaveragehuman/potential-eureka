36507e4178bbe4f97e2e22c47b8f27a16a26eff1
Jeffrey Lin <jeffrey@icurse.nl>
2019-01-10 11:12:13 -0500

    Add stuff

--- src/App.js
+++ src/App.js
@@ -1,28 +1,48 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Card from './Card';
+import axios from 'axios';
+import apikey from './api'
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+
+    getData(path, func) {
+        let api = 'https://api.giphy.com/v1/gifs/';
+        api += path;
+        api += `?api_key=${apikey}`
+        axios.get(api).then((response) => func(response));
+    }
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            endpoint: 'trending',
+            gifs: [],  // list of URLs
+        }
+    }
+
+    render() {
+        this.getData(this.state.endpoint, (response) => {
+            let objs = response.data.data;
+            for (let i of objs) {
+                axios
+                    .get(i.embed_url)
+                    .then((response) => {
+                        let old = this.state.gifs;
+                        old.push(i.embed_url);
+                        this.setState({gifs: old});
+                    })
+            }
+        })
+
+            console.log(this.state.gifs);
+        return (
+            <div className="App">
+                {this.state.gifs.map((element) => <Card gif={element} />)}
+            </div>
+        );
+    }
 }
 
 export default App;
